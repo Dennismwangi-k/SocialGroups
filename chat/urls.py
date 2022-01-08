2aae3e839d0f20442dedc278061d33c472000203
Dennis <denixxdenixx64@gmail.com>
2022-01-08 15:03:10 +0300

    chatapp

---
+++ chat/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.loginPage, name="login"),
+    path('register/', views.registerPage, name="register"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('home/', views.home, name="home"),
+    path('room/<str:pk>/',  views.room, name="room"),
+    path('profile/<str:pk>/',  views.userProfile, name="profile"),
+
+    path('create-room/',  views.CreateRoom, name="create-room"),
+    path('update-room/<str:pk>/',  views.UpdateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.DeleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    path('update_user/', views.updateuser, name="update-user"),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
+
+]
