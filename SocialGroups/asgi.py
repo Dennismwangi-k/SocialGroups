d8318db44ad72379a6b493488e1c8dbda54c1e51
Dennis <denixxdenixx64@gmail.com>
2022-01-09 12:01:12 +0300

    SocialGroups

--- SocialGroups/asgi.py
+++ SocialGroups/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for LYNNOH project.
+ASGI config for SocialGroups project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LYNNOH.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SocialGroups.settings')
 
 application = get_asgi_application()
